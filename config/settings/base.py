18821d220edfe0223817c3bb6cdd5eca2bf3ae8b
호연지 <ghduswl1214@gmail.com>
2023-01-09 17:25:39 +0900

    로그 등록

--- config/settings/base.py
+++ config/settings/base.py
@@ -189,5 +189,5 @@ LOGGING = {
             'level': 'INFO',
 
         },
-    }
+    },
 }
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -5,6 +5,7 @@ from django.db.models import Q, Count
 from ..models import Question
 import logging
 logger = logging.getLogger('pybo')
+
 def index(request):
     logger.info("INFO 레벨로 출력")
     """
